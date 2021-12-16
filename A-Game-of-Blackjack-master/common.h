186ea42632b6e2fc12b2c9c1640f448014e74c90
tranthangduc <66128389+tranthangduc@users.noreply.github.com>
2021-12-16 10:55:07 +0700

    Add files via upload

---
+++ A-Game-of-Blackjack-master/common.h
@@ -0,0 +1,25 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+
+#define MAX_PLAYERS 3
+#define BUFFER_SIZE 4000
+
+struct cards
+{
+	char name[40];
+	int number;
+};
+
+void error(const char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(1);
+}
